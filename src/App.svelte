26b6e758d2527c049af1a538864da7bce5b882fa
diamont5430 <145464000+diamont5430@users.noreply.github.com>
2023-11-14 15:38:30 +0000

    css-zmiana

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { HfInference } from "@huggingface/inference"; // importujemy potrzebne narzędzie z biblioteki HuggingFace
   let userInput: string;
+  let imagePromise: Promise<string>;
   const hf = new HfInference(import.meta.env.VITE_HUGGING_FACE_ACCESS_TOKEN); // konfigurujemy narzędzie z naszym kluczem dostępu
 
   async function ask() {
@@ -11,12 +12,41 @@
     });
     return URL.createObjectURL(blob);
   }
+  function generateImage(){
+    imagePromise = ask();
+  }
 </script>
 
 <main>
     <!-- Tutaj wpisz własny kod układu strony -->
+    <input bind:value={userInput} />
+    <button on:click={generateImage}>Generate</button>
+    {#await imagePromise}
+      Loading...
+    {:then result}
+      <img src={result}/>
+    {:catch}
+      OPPS! Failed to generate the image
+    {/await}
 </main>
 
 <style>
     /* Tu znajdzie się kod odpowiadający za wygląd strony */
+    main{
+      background-color: rgb(64, 79, 216);
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      width:100%;
+      height: 100%;
+    }
+
+    input{
+      padding: 4px;
+      border: solid;
+      border-radius: 10px;
+      text-align: center;
+    }
 </style>
